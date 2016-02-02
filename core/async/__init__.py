a203900f32eb2bcd4ef63bb9cc9f9a0c6e43ebb0
yalin.du <yalin.du@ele.me>
2016-02-02 15:25:02 +0800

    rollback code

--- core/async/__init__.py
+++ core/async/__init__.py
@@ -1,37 +1,37 @@
-import asyncio
-import random
-import time
-
-total = 0
-
-
-async def io(x, y):
-    global total
-    s = time.time()
-    await asyncio.sleep(random.random())
-    print(x + y)
-    print(time.time() - s)
-    total += (time.time() - s)
-
-
-@asyncio.coroutine
-def io_deco(x, y):
-    global total
-    s = time.time()
-    yield from asyncio.sleep(random.random())
-    print(x + y)
-    print(time.time() - s)
-    total += (time.time() - s)
-
-
-loop = asyncio.get_event_loop()
-tasks = [
-    asyncio.ensure_future(io(1, 2)),
-    asyncio.ensure_future(io(2, 3)),
-    asyncio.ensure_future(io_deco(2, 4))
-]
-try:
-    loop.run_until_complete(asyncio.wait(tasks))
-    print(total)
-finally:
-    loop.close()
+# import asyncio
+# import random
+# import time
+#
+# total = 0
+#
+#
+# async def io(x, y):
+#     global total
+#     s = time.time()
+#     await asyncio.sleep(random.random())
+#     print(x + y)
+#     print(time.time() - s)
+#     total += (time.time() - s)
+#
+#
+# @asyncio.coroutine
+# def io_deco(x, y):
+#     global total
+#     s = time.time()
+#     yield from asyncio.sleep(random.random())
+#     print(x + y)
+#     print(time.time() - s)
+#     total += (time.time() - s)
+#
+#
+# loop = asyncio.get_event_loop()
+# tasks = [
+#     asyncio.ensure_future(io(1, 2)),
+#     asyncio.ensure_future(io(2, 3)),
+#     asyncio.ensure_future(io_deco(2, 4))
+# ]
+# try:
+#     loop.run_until_complete(asyncio.wait(tasks))
+#     print(total)
+# finally:
+#     loop.close()
