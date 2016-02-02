a57430443aaba0f94ae0fd92bc13da8acb84ce4c
yalin.du <yalin.du@ele.me>
2016-02-02 15:18:11 +0800

    test for ci

--- core/async/__init__.py
+++ core/async/__init__.py
@@ -1,15 +1,27 @@
 import asyncio
 import random
+import time
+
+total = 0
+
 
 async def io(x, y):
+    global total
+    s = time.time()
     await asyncio.sleep(random.random())
     print(x + y)
+    print(time.time() - s)
+    total += (time.time() - s)
 
 
 @asyncio.coroutine
 def io_deco(x, y):
-    yield from asyncio.create_subprocess_exec
+    global total
+    s = time.time()
+    yield from asyncio.sleep(random.random())
     print(x + y)
+    print(time.time() - s)
+    total += (time.time() - s)
 
 
 loop = asyncio.get_event_loop()
@@ -17,10 +29,9 @@ tasks = [
     asyncio.ensure_future(io(1, 2)),
     asyncio.ensure_future(io(2, 3)),
     asyncio.ensure_future(io_deco(2, 4))
-
 ]
 try:
     loop.run_until_complete(asyncio.wait(tasks))
-
+    print(total)
 finally:
     loop.close()
